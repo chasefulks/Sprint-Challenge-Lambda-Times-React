26c105bbe734e9bb6892281e235605c545844a48
chasefulks <chase.fulks@gmail.com>
2018-11-16 11:16:14 -0600

    header

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+ const ChildTab = styled.div`
+  display: flex;
+  justify-content: none;
+  align-items: center;
+  Flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  margin: 0 5px;
+  padding: 2px 10px;
+  font-size: 12px;
+  letter-spacing: 2px;
+  cursor: pointer;
+  font-weight: bold;
+  &:hover {
+    text-decoration: underline;
+  }
+   ${props => props.notClassName ? `background-color: #fff; color: #333; border: 2px solid #333;` : null}
+`;
 const Tab = props => {
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
